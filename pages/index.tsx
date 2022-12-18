48b6e7e36c64a5c6e0c470bf6b438f5fe6d175d0
Ramtin Khalatbari <VERSAYANA@outlook.com>
2022-12-18 10:45:24 +0330

    Add daisyui

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ export default function Home() {
       </Head>
       <main>
         <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+        <button className="btn">Test</button>
+        <input type="checkbox" className="toggle" checked />
       </main>
     </>
   )
