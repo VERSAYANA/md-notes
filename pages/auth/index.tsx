d906f8ef33cdb283af1c464a26f623e73997af3a
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-03 22:39:55 +0330

    Redirect to "/" after login

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,9 +1,20 @@
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 function AuthPage() {
   const session = useSession()
   const supabase = useSupabaseClient()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session?.user) {
+      router.push('/')
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [session])
+
   return (
     <main className="container mx-auto flex justify-center">
       <div className="flex w-full max-w-2xl items-center justify-center p-4">
