6098cf4958152d448ff4a14fd064bafeeda0147b
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-03 22:39:23 +0330

    Save username in lowercase

--- components/Account.tsx
+++ components/Account.tsx
@@ -64,7 +64,7 @@ export default function Account({ session }: { session: Session }) {
 
       const updates = {
         id: user.id,
-        username,
+        username: username?.toLocaleLowerCase(),
         website,
         avatar_url,
         updated_at: new Date().toISOString(),
