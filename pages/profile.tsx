7e944a21e357cfe032e533e1abc1ff1314f9fcd4
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-05 19:57:44 +0330

    Add profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,34 @@
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import Account from '../components/Account'
+
+function Profile() {
+  const session = useSession()
+  const supabase = useSupabaseClient()
+  const router = useRouter()
+  console.log(session)
+
+  useEffect(() => {
+    async function getUser() {
+      const { data, error } = await supabase.auth.getUser()
+      if (!data || error) {
+        router.push('/auth')
+      }
+    }
+    getUser()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (session) {
+    return (
+      <div className="container mx-auto flex max-w-4xl flex-1 flex-col p-4 pt-8">
+        <Account session={session} />
+      </div>
+    )
+  } else {
+    return null
+  }
+}
+
+export default Profile
