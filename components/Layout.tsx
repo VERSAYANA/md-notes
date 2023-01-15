ffce8cc119bafafcd595cd9c4dbbe9d16c823582
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-15 20:28:11 +0330

    Add user management functionality to sidebar
    - Added a link to view all notes for logged-in users
    - Included a sign-in button for users that are not logged in
    - Included a link to the user's profile, an option to edit the profile,
    and a logout button for logged-in users

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode, useEffect, useState } from 'react'
 
 import Drawer from './Drawer'
 import Link from 'next/link'
-import { FilePlus } from 'react-feather'
+import { Edit, Edit2, FilePlus, LogIn, LogOut, User } from 'react-feather'
 import { useUser } from '@supabase/auth-ui-react/dist/esm/src/components/Auth/UserContext'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import { Database } from '../utils/database.types'
@@ -23,6 +23,8 @@ export default function Layout({ children }: Props) {
   const [tagsWithNoteCount, setTagsWithNoteCount] = useState<TagsWithNoteCount>(
     []
   )
+  const [username, setUsername] = useState<string>('')
+  const [userNotesCount, setUserNotesCount] = useState(0)
   async function getUserTagsWithNotesCount(userId: string) {
     try {
       console.log(userId)
@@ -42,8 +44,46 @@ export default function Layout({ children }: Props) {
     }
   }
 
+  async function getUserNotesCount(userId: string) {
+    try {
+      const { data, error } = await supabase
+        .from('notes')
+        .select('id')
+        .eq('user_id', userId)
+
+      if (error) throw error
+
+      console.log(data)
+
+      if (data) setUserNotesCount(data.length)
+
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function getUsername(userId: string) {
+    try {
+      const { data, error } = await supabase
+        .from('profiles')
+        .select('username')
+        .eq('id', userId)
+        .single()
+
+      console.log(data)
+
+      if (error) throw error
+      if (data) setUsername(data.username as string)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   useEffect(() => {
     if (session?.user) {
+      getUsername(session.user.id)
+      getUserNotesCount(session.user.id)
       getUserTagsWithNotesCount(session.user.id)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -65,42 +105,96 @@ export default function Layout({ children }: Props) {
               href="/notes/new"
             >
               <FilePlus size={24} />
-              <span>New note</span>
+              <span className="font-normal">New note</span>
             </Link>
           </div>
           <div className="flex h-full flex-1 flex-col overflow-y-auto border-y">
-            <h4 className="px-8 pt-4 pb-2 text-base font-bold">My notes</h4>
-            <ul className="menu bg-base-100 px-4 text-base-content">
-              {tagsWithNoteCount.map((tagWithNoteCount) => (
-                <li key={tagWithNoteCount.tag_name}>
+            {session?.user && (
+              <>
+                <h4 className="px-8 pt-4 pb-2 text-base font-bold">My notes</h4>
+                <ul className="menu bg-base-100 px-4 text-base-content">
+                  <li>
+                    <Link
+                      className="flex justify-between text-base-content"
+                      href={`/${username}/notes`}
+                    >
+                      <span>All</span>
+                      <span className="opacity-75">{userNotesCount}</span>
+                    </Link>
+                  </li>
+                  {tagsWithNoteCount.map((tagWithNoteCount) => (
+                    <li key={tagWithNoteCount.tag_name}>
+                      <Link
+                        className="flex justify-between text-base-content"
+                        href={`/${username}/notes/tag/${tagWithNoteCount.tag_name}`}
+                      >
+                        <span>{tagWithNoteCount.tag_name}</span>
+                        <span className="opacity-75">
+                          {tagWithNoteCount.notes_count}
+                        </span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
+          </div>
+          <div className={`flex flex-col ${session?.user ? 'h-48' : 'h-32'}`}>
+            {session?.user ? (
+              <>
+                {/* <div className="flex h-16 w-full items-center gap-x-3 px-8 text-base">
+                  <User size={24} />
+                  <span className="uppercase">{username || ''}</span>
+                </div> */}
+                <div className="flex h-16 w-full items-center px-4">
                   <Link
-                    className="flex justify-between text-base-content"
-                    href="/notes/new"
+                    className="btn btn-ghost w-full justify-start  gap-x-3 text-base normal-case text-primary"
+                    href={username ? `/${username}` : `/profile`}
                   >
-                    <span>{tagWithNoteCount.tag_name}</span>
-                    <span className="opacity-75">
-                      {tagWithNoteCount.notes_count}
+                    <User size={24} />
+                    <span className="font-normal uppercase">
+                      {username || ''}
                     </span>
                   </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="flex h-32 flex-col">
-            <ul className="menu bg-base-100 p-4 text-base-content">
-              <li>
-                <Link className="text-primary" href="/notes/new">
-                  <FilePlus size={24} />
-                  <span>New note</span>
-                </Link>
-              </li>
-              <li>
-                <Link className="text-primary" href="/notes/new">
-                  <FilePlus size={24} />
-                  <span>New note</span>
-                </Link>
-              </li>
-            </ul>
+                </div>
+                <div className="flex h-16 w-full items-center px-4">
+                  <Link
+                    className="btn btn-ghost w-full justify-start  gap-x-3 text-base normal-case text-primary"
+                    href="/profile"
+                  >
+                    <Edit2 size={24} />
+                    <span className="font-normal normal-case">
+                      Edit profile
+                    </span>
+                  </Link>
+                </div>
+                <div className="flex h-16 w-full items-center px-4">
+                  <Link
+                    className="btn btn-ghost w-full justify-start  gap-x-3 text-base normal-case text-primary"
+                    href="/auth"
+                  >
+                    <LogOut size={24} />
+                    <span className="font-normal">Log out</span>
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="flex h-16 w-full items-center gap-x-3 px-8 text-base">
+                  <User size={24} />
+                  <span className="uppercase">Anonymous</span>
+                </div>
+                <div className="flex h-16 w-full items-center px-4">
+                  <Link
+                    className="btn btn-ghost w-full justify-start  gap-x-3 text-base normal-case text-primary"
+                    href="/auth"
+                  >
+                    <LogIn size={24} />
+                    <span className="font-normal">Sign in / Sing up</span>
+                  </Link>
+                </div>
+              </>
+            )}
           </div>
         </aside>
       </div>
